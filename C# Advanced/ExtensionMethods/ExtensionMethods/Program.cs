9469d955713d0b4fe2bbb5d3a9b5e220102ddc90
Angie Siu <asiu@mscgroupny.com>
2017-02-13 16:50:30 -0500

    ExtensionMethods

---
+++ C# Advanced/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very long post blah blah blah...";
+            var shortenedPost = post.Shorten(5);
+
+            Console.WriteLine(shortenedPost);
+        }
+    }
+}
---
+++ C# Advanced/ExtensionMethods/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    //by convention extension classes must be static
+    //if they are in a separate namespace, must reference that namespace
+    //name of class is the class you are extending + "Extensions"
+    public static class StringExtensions
+    {
+        //the extension method's 1st arg is the class you are extending
+        //the args after that are whatever you need to pass in
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException("numberOfWords should be >= 0");
+
+            if (numberOfWords == 0)
+                return "";
+
+            var words = str.Split(' ');
+            if (words.Length <= numberOfWords)
+                return str;
+
+            return string.Join(" ", words.Take(numberOfWords)) + "...";
+        }
+    }
+}
