93fe77a6e388e783989add7f6c7bb5cb31c599a5
Angie Siu <asiu@mscgroupny.com>
2017-02-13 17:28:18 -0500

    LINQ

---
+++ C# Advanced/LINQ/LINQ/BookRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() {Title = "ADO.NET Step by Step", Price = 5},
+                new Book() {Title = "ASP.NET MVC", Price = 9.99f},
+                new Book() {Title = "ASP.NET Web API", Price = 12},
+                new Book() {Title = "C# Advanced Topics", Price = 7},
+                new Book() {Title = "C# 101", Price = 9},
+                new Book() {Title = "ASP.NET & AngularJS", Price = 22}
+            };
+        }
+    }
+}
---
+++ C# Advanced/LINQ/LINQ/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LINQ;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+
+            // ------------------------------
+            // LINQ Query Operators
+            // ------------------------------
+            var cheaperBooks = from b 
+                               in books
+                               where b.Price < 10
+                               orderby b.Title
+                               select b.Title;
+
+
+            // ------------------------------
+            // LINQ Extension Methods
+            // ------------------------------
+            Console.WriteLine("|---------------Cheap Books---------------|");
+            
+            var cheapBooks = books.Where(b => b.Price < 10);
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title + " : $" + book.Price);
+            }
+
+
+            Console.WriteLine("|---------------Expensive Books by Title---------------|");
+            var sortedExpensiveBooksByTitle = books
+                                                .Where(b => b.Price >= 10)
+                                                .OrderBy(b => b.Title);
+
+            foreach (var book in sortedExpensiveBooksByTitle)
+            {
+                Console.WriteLine(book.Title + " : $" + book.Price);
+            }
+
+        }
+    }
+}
