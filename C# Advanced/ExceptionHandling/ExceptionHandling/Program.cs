f65a85f216dff6c98e83e2d12e291230e2bedfe2
Angie Siu <asiu@mscgroupny.com>
2017-02-14 11:59:42 -0500

    ExceptionHandling -- try-catch with using

--- C# Advanced/ExceptionHandling/ExceptionHandling/Program.cs
+++ C# Advanced/ExceptionHandling/ExceptionHandling/Program.cs
@@ -11,22 +11,18 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
-            StreamReader streamReader = null; //init streamReader
             try
             {
-                streamReader = new StreamReader(@"C:\file.zip");
-                var content = streamReader.ReadToEnd();
+                using (var streamReader = new StreamReader(@"C:\file.zip"))
+                {
+                    var content = streamReader.ReadToEnd();
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Sorry, an error occurred.");
             }
-            //finally blocks execute no matter what
-            finally // make sure that Stream is ALWAYS closed
-            {
-                if(streamReader != null)
-                    streamReader.Dispose();
-            }
+            
         }
     }
 }
