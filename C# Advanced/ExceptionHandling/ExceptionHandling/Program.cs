7426c72e0fddeb6cc9245fbfd1ff94f40cd37d25
Angie Siu <asiu@mscgroupny.com>
2017-02-14 11:49:51 -0500

    ExceptionHandling -- DivideByZeroException

---
+++ C# Advanced/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var calculator = new Calculator();
+                var result = calculator.Divide(5, 0);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Cannot divide by zero!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Sorry, error occurred");
+            }
+        }
+    }
+}
